a5de253eba04d0df844b943c60ba6f34068a7ca7
Ram Nayan <ram.nayan@antiersolutions.com>
2024-05-21 17:03:11 +0530

    test case for empty string

--- spec/tdd_calculator_spec.rb
+++ spec/tdd_calculator_spec.rb
@@ -1,5 +1,13 @@
 require 'tdd_calculator'
 
 RSpec.describe TddCalculator do
-  
+  describe '#add' do
+    let(:calculator) { TddCalculator.new }
+
+    context 'handle for empty string' do
+      it 'returns 0' do
+        expect(calculator.add('')).to eq(0)
+      end
+    end
+  end
 end

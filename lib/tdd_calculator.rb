6f086d3cfcfb44e18c71fe4cce67b6b4ea7cb81f
Ram Nayan <ram.nayan@antiersolutions.com>
2024-05-21 21:11:06 +0530

    return 0 if empty string and test case for 1, 2 and multiple number of numbers

--- lib/tdd_calculator.rb
+++ lib/tdd_calculator.rb
@@ -1,3 +1,5 @@
 class TddCalculator
-  
+  def add(input)
+  	return 0 if input.empty?
+  end
 end
--- spec/tdd_calculator_spec.rb
+++ spec/tdd_calculator_spec.rb
@@ -9,5 +9,26 @@ RSpec.describe TddCalculator do
         expect(calculator.add('')).to eq(0)
       end
     end
+
+    context 'when the input is a single number' do
+      it 'returns that number' do
+        expect(calculator.add('1')).to eq(1)
+      end
+    end
+
+    context 'when the input is two numbers' do
+      it 'returns their sum' do
+        expect(calculator.add('1,2')).to eq(3)
+      end
+    end
+
+    context 'when the input is multiple numbers' do
+      it 'returns their sum' do
+        expect(calculator.add('1,2,3')).to eq(6)
+        expect(calculator.add('4,5,6,7')).to eq(22)
+        expect(calculator.add('10,20,30,40,50')).to eq(150)
+      end
+    end
+
   end
 end
